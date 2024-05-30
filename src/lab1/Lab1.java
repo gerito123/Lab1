b657843083b952f3c03b6c6f73502b6d077a0edb
gerito123 <152526870+gerito123@users.noreply.github.com>
2024-05-30 19:24:21 -0300

    Add files via upload

---
+++ src/lab1/Lab1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab1;
+
+/**
+ *
+ * @author Informatica7
+ */
+public class Lab1 {
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
